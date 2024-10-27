0f95f552bbb52e6df5f71576e91e04f4043a1b12
Rahdeg <walett95@gmail.com>
2024-10-27 19:15:29 +0100

    authentication

---
+++ src/features/auth/api/use-logout.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.auth.login)["$post"],
+  200
+>;
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.auth.logout["$post"]();
+      return await response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+  });
+
+  return mutation;
+};
