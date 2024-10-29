512dac6e00fe895a957e9def731c6acc5867921f
Rahdeg <walett95@gmail.com>
2024-10-29 01:44:11 +0100

    den;

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
 
-const DashboardPage = () => {
-    return (
-        <div>DashboardPage</div>
-    )
+import { UserButton } from "@/features/auth/components/user-button";
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
+
+
+
+export default async function Home() {
+
+  const user = await getCurrent();
+
+
+  if (!user) redirect("/sign-in");
+
+
+
+  return (
+    <div className="">
+      <UserButton />
+    </div>
+  );
 }
-
-export default DashboardPage
